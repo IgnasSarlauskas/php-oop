819932ce14d191eee56cc4cf4368c635acf341b3
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-22 12:47:45 +0300

    #X save function

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -3,25 +3,34 @@
 class FileDB {
 
     private $file_name;
+
+    /** @var $data array Duomenu masyvas sugeneruotas is failo */
     private $data;
 
     /**
-     *
-     * Calls when new object is created
-     *
-     * @param   $file_name
-     * @return  string value
-     *
+     * Funkcija, kuri i klase paduoda failo pavadinima 
      */
     public function __construct($file_name) {
         $this->file_name = $file_name;
     }
 
+    /**
+     * F-ija, kuri užkrauna duomenis iš failo į $this->data
+     */
     public function load() {
         $this->data = file_to_array($this->file_name);
     }
     
+    /**
+     * F-ija, nustatanti duomenų masyvą
+     * @param type $data_array
+     */
     public function setData($data_array) {
         $this->data = $data_array;
     }
+    
+    public function save() {
+        $this->data = array_to_file($this->file_name);
+    }
+
 }
