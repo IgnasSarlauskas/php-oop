d85ea6d0bf7e30b2a3b90a46e4da559b7bf5608f
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-22 12:41:50 +0300

    #2 setData

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,19 +1,27 @@
 <?php
 
 class FileDB {
-    
+
     private $file_name;
     private $data;
-    
-    // funkcija, kuri issikviecia kai sukuriame nauja objekta
-    public function __construct($file_name){
+
+    /**
+     *
+     * Calls when new object is created
+     *
+     * @param   $file_name
+     * @return  string value
+     *
+     */
+    public function __construct($file_name) {
         $this->file_name = $file_name;
     }
-    
+
     public function load() {
         $this->data = file_to_array($this->file_name);
     }
+    
+    public function setData($data_array) {
+        $this->data = $data_array;
+    }
 }
-
-
-
