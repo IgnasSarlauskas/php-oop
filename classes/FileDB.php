8a670666562a907f2ea7eac7a950e191c892022c
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-22 12:25:46 +0300

    #1 public function load

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,13 +1,19 @@
 <?php
 
 class FileDB {
-    private $file_name;
     
+    private $file_name;
+    private $data;
+    
+    // funkcija, kuri issikviecia kai sukuriame nauja objekta
     public function __construct($file_name){
         $this->file_name = $file_name;
     }
+    
+    public function load() {
+        $this->data = file_to_array($file_name);
+    }
 }
 
-$info = new FileDB('info');
-var_dump($info);
+
 
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
+require 'functions/file.php';
 require 'classes/FileDB.php';
+
+$info = new FileDB('info');
+var_dump($info);
