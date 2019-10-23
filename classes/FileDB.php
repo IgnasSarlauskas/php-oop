cfd817fa05ef7ce511f35d1d4253a6a90de363a5
IgnasSarlauskas <ignas.sarlauskas24@gmail.com>
2019-10-23 11:21:36 +0300

    10/23 #3

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -33,16 +33,16 @@ class FileDB {
     }
     
     public function createTable($table_name) {
-        if(!isset($this->data[$table_name])) {
-           $this->data[$table_name] = [];
-           return true;
+        if (!$this->tableExists($table_name)) {
+            $this->data[$table_name] = [];
+            return true;
         } 
         
         return false;
     }
     
     public function tableExists($table_name) {
-        if(isset($this->data[$table_name])) {
+        if (isset($this->data[$table_name])) {
             return true;
         }
         
